c08afd05e35f5d92df635ef8d1c53f7de9883c65
John Murphy <dearjohnm@gmail.com>
2017-04-14 10:26:46 -0700

    minor update to oe-theme.css; updated gitignore so that rollup config is not ignored

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+export default {
+  entry: 'dist/index.js',
+  dest: 'dist/bundles/ng2-unlv-oe.umd.js',
+  sourceMap: false,
+  format: 'umd',
+  moduleName: 'ng.unlvoe',
+  globals: {
+    '@angular/core': 'ng.core',
+    '@angular/common': 'ng.common',
+    '@angular/http': 'ng.http',
+    'rxjs/add/operator/map': 'Rx.Observable.prototype'
+  }
+}
